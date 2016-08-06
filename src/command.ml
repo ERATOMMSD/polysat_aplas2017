35e8622231bc2424bed819985a1f67804c4252ce
Yuki Nishida <rsworks.snow@gmail.com>
2016-08-06 21:25:27 +0900

    Add checks for problem file loading
    Check so that template polynomials does not slip into constraints.

--- src/command.ml
+++ src/command.ml
@@ -6,7 +6,7 @@ let var_regexp =
 let par_regexp =
   Str.regexp "'[a-z]+"
 
-let rec sexp_of_poly sexp =
+let rec sexp_of_poly ?(is_templ=false) sexp =
   match sexp with
   | Sexp.Atom i ->
       begin match int_of_string i with
@@ -14,13 +14,14 @@ let rec sexp_of_poly sexp =
           if Str.string_match var_regexp i 0 then
             Formula.Poly.var i
           else if Str.string_match par_regexp i 0 then
-            Formula.Poly.const (Formula.PPoly.var 0)
+            if is_templ then Formula.Poly.const (Formula.PPoly.var 0)
+            else Conv.of_sexp_error "template polynomial cannot be in here" sexp
           else
             Conv.of_sexp_error "(parameter) variable must be of the form (')[a-z]+" sexp
       | n -> Formula.Poly.const (Formula.PPoly.const (Num.num_of_int n))
       end
   | Sexp.List (Sexp.Atom op :: args) ->
-      let args = List.map sexp_of_poly args in
+      let args = List.map (sexp_of_poly ~is_templ) args in
       begin match op with
       | "+" -> List.fold_left Formula.Poly.add Formula.Poly.zero args
       | "-" ->
@@ -107,9 +108,16 @@ let sexp_of_t sexp =
           else if List.length args = 4 then
             let f1 = sexp_of_formula (List.nth args 0) in
             let f2 = sexp_of_formula (List.nth args 1) in
-            let template = sexp_of_poly (List.nth args 2) in
+            let template = sexp_of_poly ~is_templ:true (List.nth args 2) in
             let d = Conv.int_of_sexp (List.nth args 3) in
-            Interpolant (f1, f2, Some template, d)
+            if
+              List.exists
+                (fun (_, c) -> Formula.PPoly.degree c > 1)
+                (Formula.Poly.to_list template)
+            then
+              Conv.of_sexp_error "coefficients' polynomials must be linear polynomials" sexp
+            else
+              Interpolant (f1, f2, Some template, d)
           else
             Conv.of_sexp_error "interpolant accepts two logical formulae and degree" sexp
       | _ -> Conv.of_sexp_error "unknown command" sexp
