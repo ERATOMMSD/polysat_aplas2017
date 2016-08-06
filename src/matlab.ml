617a9286e0192f8c1262d5d367eec65411146ac4
Yuki Nishida <rsworks.snow@gmail.com>
2016-08-06 15:50:10 +0900

    Fix matlab code generator

--- src/matlab.ml
+++ src/matlab.ml
@@ -1,44 +1,46 @@
-open Util.Printf
+open Format
 
 let print_code psds zeros ip =
-  (* let syms = *)
-  (*   List.map Formula.Poly.Matrix.to_list_list psds *)
-  (*   |> List.concat |> List.concat *)
-  (* in *)
-  (* Format.printf "sdpvar %a;@\n" *)
-  (*   (Format.pp_print_list ~pp_sep:(fun fmt () -> Format.fprintf fmt " ") Formula.Poly.pp) *)
-  (*   syms; *)
-  (* printf "sdpvar %a;\n" (print_list Formula.Poly.print) syms; *)
-  (* print_newline (); *)
+  let syms =
+    List.map Formula.Poly.Matrix.to_list_list psds
+    |> List.concat |> List.concat
+  in
+  printf "@[<h>sdpvar %a;@]@\n"
+    (pp_print_list ~pp_sep:(fun fmt () -> Format.fprintf fmt " ") Formula.Poly.pp)
+    syms;
+  print_newline ();
 
-  (* let l = Util.List.count 0 (List.length psds) in *)
-  (* printf "%a\n" *)
-  (*   (print_list ~sep:"\n" *)
-  (*      (fun out (i, m) -> fprintf out "Q%d = %a;" i Formula.Poly.Matrix.print m)) *)
-  (*   (List.combine l psds); *)
-  (* print_newline (); *)
+  let l = Util.List.count 0 (List.length psds) in
+  printf "@[<v>%a@]@\n"
+    (pp_print_list
+      (fun fmt (i, m) -> fprintf fmt "@[<h>Q%d = %a;@]" i Formula.Poly.Matrix.pp m))
+    (List.combine l psds);
+  print_newline ();
 
-  (* printf "F = [%a;\n" *)
-  (*   (print_list (fun out i -> fprintf out "Q%d >= 0" i)) l; *)
-  (* printf "%a];\n" *)
-  (*   (print_list ~sep:"\n" *)
-  (*      (fun out c -> fprintf out "    %a == 0;" Formula.PPoly.print c)) zeros; *)
-  (* print_newline (); *)
+  printf "F = [@[%a;@\n"
+    (pp_print_list ~pp_sep:(fun fmt () -> Format.fprintf fmt "; ")
+       (fun fmt d -> fprintf fmt "Q%d >= 0" d))
+    l;
+  printf "@[<v>%a@]@]];@\n"
+    (pp_print_list
+       (fun fmt c -> fprintf fmt "@[<h>%a == 0;@]" Formula.PPoly.pp c))
+    zeros;
+  print_newline ();
 
-  (* printf "optimize(F);\n"; *)
-  (* print_newline (); *)
+  printf "optimize(F);@\n";
+  print_newline ();
 
-  (* let vars = Formula.Poly.vars ip |> Formula.Poly.VarSet.elements in *)
-  (* printf "sdpvar %a;\n" *)
-  (*   (print_list (fun out s -> fprintf out "%s" s)) vars; *)
-  (* print_newline (); *)
+  let vars = Formula.Poly.vars ip |> Formula.Poly.VarSet.elements in
+  printf "@[<h>sdpvar %a;@]@\n"
+    (pp_print_list ~pp_sep:(fun fmt () -> fprintf fmt " ") pp_print_string)
+    vars;
+  print_newline ();
 
-  (* printf "%a\n" *)
-  (*   (print_list ~sep:"\n" *)
-  (*      (fun out a -> *)
-  (*         fprintf out "%a = value(%a);" Formula.Poly.print a Formula.Poly.print a)) *)
-  (*   syms; *)
-  (* print_newline (); *)
+  printf "@[<v>%a@]@\n"
+    (pp_print_list
+       (fun fmt a -> fprintf fmt "@[<h>%a = value(%a);@]" Formula.Poly.pp a Formula.Poly.pp a))
+    syms;
+  print_newline ();
 
-  (* printf "ip = %a;\n" Formula.Poly.print ip; *)
+  printf "@[<h>ip = %a;@]@\n" Formula.Poly.pp ip;
   printf "sdisplay(ip);\n"
