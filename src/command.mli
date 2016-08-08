2c86e5a9df224a63485c83bdc61b584bbe44cc0f
Yuki Nishida <rsworks.snow@gmail.com>
2016-08-08 13:54:33 +0900

    Add comments

--- src/command.mli
+++ src/command.mli
@@ -1,5 +1,8 @@
+(** Command parser *)
+
 type t =
   | Simplify of Formula.t
   | Interpolant of Formula.t * Formula.t * Formula.Poly.t option * int
 
+(** [load filename] reads commands from the file [filename]. *)
 val load: string -> t list
